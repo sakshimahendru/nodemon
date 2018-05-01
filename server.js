71d5fb37798d45daf155d0f84ce9504c4446bbf6
sakshimahendru <sakshim@hike.in>
2018-05-01 15:51:39 +0530

    adding changes

--- server.js
+++ server.js
@@ -118,13 +118,13 @@ router.get('/failedPerc',function(req,res){
 
 
 //latets runs by branchVersion
-router.get('/latestruns/:version',function(req,res){
+router.get('/latestrun/:version',function(req,res){
     let version = req.params.version;
      if (!version) {
         return res.status(400).send({ error:true, message: 'Please provide version' });
     }
     pool.getConnection(function(err,connection){
-    connection.query(`Select t.branchName,t.branchVersion,t.totalCases,t.totalPass,t.totalFail,t.type,t.createdON FROM  regression_run.TestRun as t where t.branchVersion='?' order by createdON limit 5;`,[ version ],function(error,rows){
+    connection.query(`Select t.branchName,t.branchVersion,t.totalCases,t.totalPass,t.totalFail,t.type,t.createdON FROM  regression_run.TestRun as t where t.branchVersion=? order by createdON limit 5;`,[ version ],function(error,rows){
     console.log(pool._freeConnections.indexOf(connection)); // -1
     connection.release();
     console.log(pool._freeConnections.indexOf(connection)); // 0 
