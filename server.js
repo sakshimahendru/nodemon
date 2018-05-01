2c25aa00a66cad82a2d231d0d409e89a5ba02412
sakshimahendru <sakshim@hike.in>
2018-05-01 15:39:38 +0530

    adding changes

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ router.get("lastrun/:version",function(req,res){
         return res.status(400).send({ error:true, message: 'Please provide branch version' });
     }
     pool.getConnection(function(err,connection){
-    var query=connection.query('Select t.branchName,t.branchVersion,t.totalCases,t.totalPass,t.totalFail,t.type,t.createdON FROM regression_run.TestRun as t where t.branchVersion='?' order by createdON limit 5;',[ version ],function(error,rows) {
+    var query=connection.query(`Select t.branchName,t.branchVersion,t.totalCases,t.totalPass,t.totalFail,t.type,t.createdON FROM regression_run.TestRun as t where t.branchVersion='?' order by createdON limit 5;`,[ version ],function(error,rows) {
     console.log(query);
     console.log(pool._freeConnections.indexOf(connection)); // -1
     connection.release();
